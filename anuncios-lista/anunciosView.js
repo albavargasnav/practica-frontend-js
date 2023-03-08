220f018970c3358fd4312bb2043f85ea8e3e9214
Alba Vargas <118747197+albavargasnav@users.noreply.github.com>
2023-03-08 17:32:50 +0100

    add content from json

--- anuncios-lista/anunciosView.js
+++ anuncios-lista/anunciosView.js
@@ -4,11 +4,17 @@
 export function buildAnuncioView(anuncio) {
     //añadimos y creamos nodo article
     const newAnuncioElement = document.createElement('article');
+    //es la fecha 
+    const anuncioDate = new Date(anuncio.date)
+
     // generar el HTML que representará un anuncio
     //rellena el anuncio con dos article que incluyen dos <p>
     newAnuncioElement.innerHTML = ` 
-        <p>${anuncio.usuario}</p>
-        <p>${anuncio.descripcion} - ${anuncio.tipo}</p>
+        <p>${anuncio.handle}</p> 
+        // Aqui se ve el anuncio del avatar
+        <img src="${anuncio.avatar}"/> 
+        <p>${anuncio.content} - ${anuncioDate.toISOString()}</p>
+        <span></span><span></span><span></span>
     `;
 
     //devuelve un nuevo anuncio
